39bb7aff27202065b9c986a869036baa67c4dfcf
Marie J <marie.jeammet@beta.gouv.fr>
2022-12-15 16:31:50 +0100

    a11y: add declaration of noncompliance

--- yourApp/views.py
+++ yourApp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, "yourApp/index.html", {})
+
+
+def accessibility_view(request):
+    return render(request, "yourApp/accessibility.html", {})
